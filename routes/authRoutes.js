8f82bd350d6ab5d94a7d2723881d4b2b4ed9a5b2
Shriyash Aruva <shriyasharuva2005@gmail.com>
2025-08-09 17:40:01 +0530

    remote push

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { sendOtp, verifyOtp } = require('../controllers/authController');
 
+//routes
+
 router.post('/send-otp', sendOtp);
 router.post('/verify-otp', verifyOtp);
 
